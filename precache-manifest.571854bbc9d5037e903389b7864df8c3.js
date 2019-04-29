d1e1c24506237b17652671c304ebeac517d10fad
philippvlasov <vlasov.philipp@gmail.com>
2019-04-29 18:42:56 +0300

    Updates

--- precache-manifest.571854bbc9d5037e903389b7864df8c3.js
+++ precache-manifest.571854bbc9d5037e903389b7864df8c3.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/meme-generator-v2/static/js/runtime~main.85078bf1.js"
   },
   {
-    "revision": "e327dfaf01b6fe7651b6",
-    "url": "/meme-generator-v2/static/js/main.f1e72b77.chunk.js"
+    "revision": "f56c1c77619de56b9e7b",
+    "url": "/meme-generator-v2/static/js/main.24735080.chunk.js"
   },
   {
-    "revision": "f4d5acad003d847d08c1",
-    "url": "/meme-generator-v2/static/js/2.a6e951fb.chunk.js"
+    "revision": "d622c784dd829e275701",
+    "url": "/meme-generator-v2/static/js/2.eea36c8b.chunk.js"
   },
   {
-    "revision": "e327dfaf01b6fe7651b6",
+    "revision": "f56c1c77619de56b9e7b",
     "url": "/meme-generator-v2/static/css/main.eb06cdf3.chunk.css"
   },
   {
-    "revision": "f4d5acad003d847d08c1",
+    "revision": "d622c784dd829e275701",
     "url": "/meme-generator-v2/static/css/2.b45a8ab3.chunk.css"
   },
   {
-    "revision": "3cce382d2c1643254b8f16c118d729fd",
+    "revision": "f7dd037eb32bc19f0c3d24ea5206dc26",
     "url": "/meme-generator-v2/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meme-generator-v2/precache-manifest.f31914772c81ee314b450a519450be37.js"
+  "/meme-generator-v2/precache-manifest.571854bbc9d5037e903389b7864df8c3.js"
 );
 
 workbox.clientsClaim();
